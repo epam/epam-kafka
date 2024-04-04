7a582014e488daa3193965434c56640def08ead0
Ihar Yakimush <Ihar_Yakimush@epam.com>
2024-04-04 11:43:01 +0300

    Configure CI/CD pipeline #1

--- build.cmd
+++ build.cmd
@@ -1,6 +1,5 @@
-dotnet restore
-dotnet build -c Debug --no-restore
-dotnet test  -c Debug --no-build --settings .runsettings
+dotnet build -c Debug
+dotnet test  -c Debug --no-build --verbosity normal --settings .runsettings
 dotnet tool restore
 dotnet reportgenerator -reports:./artifacts/cob/*/coverage.cobertura.xml -targetdir:artifacts/html -reporttypes:HtmlInline
 dotnet reportgenerator -reports:./artifacts/cob/*/coverage.cobertura.xml -targetdir:artifacts -reporttypes:"Cobertura;HtmlSummary;TextSummary"
