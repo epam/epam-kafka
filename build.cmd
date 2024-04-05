4500be0d03679845cf877433ecffe5d0c3fee67a
IharYakimush <Ihar_Yakimush@epam.com>
2024-04-05 13:56:36 +0300

    nuget job (#4)

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 dotnet restore
 dotnet build -c Debug --no-restore
-dotnet test  -c Debug --no-build --verbosity normal --settings .runsettings --filter FullyQualifiedName!~IntegrationTests 
+dotnet test  -c Debug --no-build --verbosity normal --settings .runsettings 
 dotnet tool restore
 dotnet reportgenerator -reports:./artifacts/cob/*/coverage.cobertura.xml -targetdir:artifacts/html -reporttypes:HtmlInline
-dotnet reportgenerator -reports:./artifacts/cob/*/coverage.cobertura.xml -targetdir:artifacts -reporttypes:"Cobertura;HtmlSummary;TextSummary"
-dotnet pack -c Release --output ./artifacts/nuget
+dotnet reportgenerator -reports:./artifacts/cob/*/coverage.cobertura.xml -targetdir:artifacts -reporttypes:"Cobertura;HtmlSummary"
+dotnet pack -c Release -p:VersionPrefix=0-rc-local --output ./artifacts/nuget
