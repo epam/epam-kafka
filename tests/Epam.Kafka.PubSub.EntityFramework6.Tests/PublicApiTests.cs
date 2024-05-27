0c68c567ad42bfc792e1a0419808937d6a9fd96f
Ihar Yakimush <Ihar_Yakimush@epam.com>
2024-05-27 15:31:08 +0300

    refactor ef6 tests

---
+++ tests/Epam.Kafka.PubSub.EntityFramework6.Tests/PublicApiTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2024 EPAM Systems
+
+
+using Epam.Kafka.PubSub.EntityFramework6.Subscription.State;
+
+using Epam.Kafka.Tests.Common;
+
+using Xunit;
+
+namespace Epam.Kafka.PubSub.EntityFramework6.Tests;
+
+public class PublicApiTests
+{
+    [Fact]
+    public void ApiDifferenceTests()
+    {
+        typeof(KafkaTopicState).Assembly.ShouldMatchApproved();
+    }
+}
