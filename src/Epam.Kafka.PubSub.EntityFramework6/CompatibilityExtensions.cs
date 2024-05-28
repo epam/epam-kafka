9085421d67e9e00886beaab6f2bdc9c6a431fee6
Ihar Yakimush <Ihar_Yakimush@epam.com>
2024-05-28 10:30:49 +0300

    EF6 Subscription handler

--- src/Epam.Kafka.PubSub.EntityFramework6/CompatibilityExtensions.cs
+++ src/Epam.Kafka.PubSub.EntityFramework6/CompatibilityExtensions.cs
@@ -11,6 +11,19 @@ internal static class CompatibilityExtensions
     public static IQueryable<T> AsTracking<T>(this IQueryable<T> queryable) => queryable;
 
     public static int SaveChanges(this DbContext context, bool acceptAllChangesOnSuccess) => context.SaveChanges();
+    public static void Add(this DbContext context, object entity)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+        context.Set(entity.GetType()).Add(entity);
+    }
+
+    public static void Remove(this DbContext context, object entity)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+        context.Set(entity.GetType()).Remove(entity);
+    }
 }
 
 #endif
