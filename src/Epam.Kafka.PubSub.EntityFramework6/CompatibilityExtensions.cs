bbe939cabc0db32565ebe133f2e56bb1e7db7ca5
Ihar Yakimush <Ihar_Yakimush@epam.com>
2024-05-28 10:25:49 +0300

    EF 6 Sub offsets

---
+++ src/Epam.Kafka.PubSub.EntityFramework6/CompatibilityExtensions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using System.Data.Entity;
+
+#if EF6
+
+namespace Epam.Kafka.PubSub.EntityFramework6;
+
+internal static class CompatibilityExtensions
+{
+    public static IQueryable<T> AsTracking<T>(this IQueryable<T> queryable) => queryable;
+
+    public static int SaveChanges(this DbContext context, bool acceptAllChangesOnSuccess) => context.SaveChanges();
+}
+
+#endif
