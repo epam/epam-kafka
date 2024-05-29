f30fbd8015cd7624f4cd17418ef6a8a7180bc9a3
Ihar Yakimush <Ihar_Yakimush@epam.com>
2024-05-29 12:28:52 +0300

    net462 sample project

---
+++ sample/Epam.Kafka.Sample.Net462/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Extensions.Hosting;
+
+namespace Epam.Kafka.Sample.Net462
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+
+            }).Build().Run();
+        }
+    }
+}
