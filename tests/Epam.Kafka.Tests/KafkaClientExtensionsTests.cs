44bb74a0b8762283ff5ef99962053fa2ab82caae
IharYakimush <Ihar_Yakimush@epam.com>
2024-06-10 11:00:09 +0300

    Tests1 (#17)

---
+++ tests/Epam.Kafka.Tests/KafkaClientExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using Xunit;
+
+namespace Epam.Kafka.Tests;
+
+public class KafkaClientExtensionsTests
+{
+    [Fact]
+    public void ArgumentExceptions()
+    {
+        Assert.Throws<ArgumentNullException>(() => KafkaClientExtensions.CreateDependentAdminClient(null!));
+        Assert.Throws<ArgumentNullException>(() => KafkaClientExtensions.CreateDependentProducer<int, int>(null!));
+    }
+}
---
+++ tests/Epam.Kafka.Tests/OAuthRefreshResultTests.cs
@@ -0,0 +1,29 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using Xunit;
+
+namespace Epam.Kafka.Tests;
+
+public class OAuthRefreshResultTests
+{
+    [Fact]
+    public void ArgumentExceptions()
+    {
+        Assert.Throws<ArgumentNullException>(() => new OAuthRefreshResult(null!, DateTimeOffset.Now, "qwe"));
+        Assert.Throws<ArgumentNullException>(() => new OAuthRefreshResult("qwe", DateTimeOffset.Now, null!));
+    }
+
+    [Fact]
+    public void Arguments()
+    {
+        DateTimeOffset anyDto = DateTimeOffset.Now;
+        string anyToken = "t";
+        string anyPrincipalName = "pn";
+
+        OAuthRefreshResult r1 = new(anyToken, anyDto, anyPrincipalName);
+
+        Assert.Equal(anyToken, r1.TokenValue);
+        Assert.Equal(anyDto, r1.ExpiresAt);
+        Assert.Equal(anyPrincipalName, r1.PrincipalName);
+    }
+}
