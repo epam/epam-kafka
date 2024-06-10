d65d2f87b7bf13bd499a87e768ef5819d0231a00
Ihar Yakimush <Ihar_Yakimush@epam.com>
2024-06-10 10:20:15 +0300

    KafkaBuilderTests

---
+++ tests/Epam.Kafka.Tests/KafkaBuilderTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Epam.Kafka.Tests;
+
+public class KafkaBuilderTests
+{
+    [Fact]
+    public void ArgumentExceptions()
+    {
+        KafkaBuilder kafkaBuilder = new KafkaBuilder(new ServiceCollection(), false);
+
+        Assert.Throws<ArgumentNullException>(() => kafkaBuilder.WithClusterConfig(null!));
+        Assert.Throws<ArgumentNullException>(() => kafkaBuilder.WithProducerConfig(null!));
+        Assert.Throws<ArgumentNullException>(() => kafkaBuilder.WithConsumerConfig(null!));
+    }
+}
