20ed856053a89dfd6629283b74cb1985a8662d75
Ihar Yakimush <Ihar_Yakimush@epam.com>
2024-06-26 12:00:45 +0300

    Stats tests

---
+++ tests/Epam.Kafka.Tests/StatisticsTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using Shouldly;
+using Xunit;
+
+namespace Epam.Kafka.Tests;
+
+public class StatisticsTests
+{
+    [Fact]
+    public void ParseErrors()
+    {
+        Assert.Throws<ArgumentNullException>(() => Statistics.FromJson(null!));
+        Assert.Throws<ArgumentException>(() => Statistics.FromJson(""));
+        Assert.Throws<ArgumentException>(() => Statistics.FromJson("not a json"));
+    }
+
+    [Fact]
+    public void ParseOk()
+    {
+        Statistics value = Statistics.FromJson(
+@"{
+    ""txmsgs"": 5,
+    ""rxmsgs"": 4
+  }");
+
+        value.TransmittedMessagesTotal.ShouldBe(5);
+        value.ConsumedMessagesTotal.ShouldBe(4);
+    }
+}
