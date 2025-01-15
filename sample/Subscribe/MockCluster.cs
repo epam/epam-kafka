a9f4164831b03aec2b51f6c72c02b6ebf6338608
Ihar Yakimush <Ihar_Yakimush@epam.com>
2025-01-15 15:33:51 +0300

    subscribe sample

---
+++ sample/Subscribe/MockCluster.cs
@@ -0,0 +1,75 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using Confluent.Kafka;
+using Epam.Kafka;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Subscribe;
+
+public sealed class MockCluster : IDisposable
+{
+    private readonly Lazy<IProducer<byte[], byte[]?>> _producerLazy;
+
+    private readonly ILogger _logger;
+
+    public MockCluster(int numBrokers, ILoggerFactory? loggerFactory = null)
+    {
+        if (numBrokers <= 0) throw new ArgumentOutOfRangeException(nameof(numBrokers));
+        if (numBrokers > 3) throw new ArgumentOutOfRangeException(nameof(numBrokers));
+
+        this._logger = loggerFactory?.CreateLogger(typeof(MockCluster)) ?? NullLogger.Instance;
+
+        this._producerLazy =
+            new(() =>
+            {
+                ProducerConfig config = new ProducerConfig
+                {
+                    AllowAutoCreateTopics = true,
+                    BootstrapServers = "localhost:9200"
+                };
+
+                config.Set("test.mock.num.brokers", $"{numBrokers}");
+
+                ProducerBuilder<byte[], byte[]?> pb = new(config);
+
+                pb.SetLogHandler((_, message) => this._logger.KafkaLogHandler(message));
+
+                return pb.Build();
+            }, LazyThreadSafetyMode.ExecutionAndPublication);
+    }
+
+    public string BootstrapServers
+    {
+        get
+        {
+            using IAdminClient adminClient = this._producerLazy.Value.CreateDependentAdminClient();
+
+            var metadata = adminClient.GetMetadata(TimeSpan.FromSeconds(1));
+
+            return string.Join(", ", metadata.Brokers.Select(x => $"{x.Host}:{x.Port}"));
+        }
+    }
+
+    public async Task<DeliveryResult<byte[], byte[]?>> SeedTopicAsync(string topicName, byte[] key, byte[]? value, Headers? headers = null)
+    {
+        Message<byte[], byte[]?> message = new Message<byte[], byte[]?>
+        {
+            Key = key,
+            Value = value,
+            Headers = headers
+        };
+
+        DeliveryResult<byte[], byte[]?> result = await this._producerLazy.Value.ProduceAsync(topicName, message);
+
+        return result;
+    }
+
+    void IDisposable.Dispose()
+    {
+        if (this._producerLazy.IsValueCreated)
+        {
+            this._producerLazy.Value.Dispose();
+        }
+    }
+}
---
+++ sample/Subscribe/Program.cs
@@ -0,0 +1,65 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using Confluent.Kafka;
+
+using Epam.Kafka;
+using Epam.Kafka.PubSub;
+using Epam.Kafka.PubSub.Subscription;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Subscribe;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+
+        hostBuilder.ConfigureServices((context, services) =>
+        {
+            KafkaBuilder builder = services.AddKafka();
+
+            services.AddSingleton(sp => new MockCluster(1, sp.GetService<ILoggerFactory>()));
+            // use mock cluster for demo purposes only, NOT for production!
+            builder.WithClusterConfig("Sandbox").PostConfigure<MockCluster>((options, mock) =>
+            {
+                options.ClientConfig.BootstrapServers = mock.BootstrapServers;
+            });
+
+            string environmentName = context.HostingEnvironment.EnvironmentName;
+
+            builder.WithConfigPlaceholders($"<{nameof(context.HostingEnvironment.EnvironmentName)}>", environmentName);
+
+            // subscription summary health check with "live" tag
+            builder.WithSubscriptionSummaryHealthCheck(new[] { "live" });
+
+            // add subscription with default offsets storage managed by kafka broker
+            builder.AddSubscription<Ignore, Ignore, SubSample>(nameof(SubSample))
+                .WithHealthChecks()
+
+                // seed topic before processing
+                .WaitFor(sp => sp.GetRequiredService<MockCluster>().SeedTopicAsync("sample.name", new Guid().ToByteArray(), new Guid().ToByteArray()));
+
+        }).Build().Run();
+    }
+}
+
+public class SubSample : ISubscriptionHandler<Ignore, Ignore>
+{
+    private readonly ILogger<SubSample> _logger;
+
+    public SubSample(ILogger<SubSample> logger)
+    {
+        this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+    public void Execute(IReadOnlyCollection<ConsumeResult<Ignore, Ignore>> items, CancellationToken cancellationToken)
+    {
+        foreach (ConsumeResult<Ignore, Ignore> result in items)
+        {
+            this._logger.LogInformation("Processed {Tpo}", result.TopicPartitionOffset);
+        }
+    }
+}
