2b89b73fa4a3dcc5e78964045877df4ee3d0621b
Ihar Yakimush <Ihar_Yakimush@epam.com>
2025-01-24 13:43:14 +0300

    Strongly typed statistics #60

---
+++ src/Epam.Kafka/Stats/BrokerStatistics.cs
@@ -0,0 +1,54 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using System.Text.Json.Serialization;
+using Confluent.Kafka;
+
+namespace Epam.Kafka.Stats;
+
+/// <summary>
+/// Per broker statistics. See https://github.com/confluentinc/librdkafka/blob/master/STATISTICS.md for details.
+/// </summary>
+public class BrokerStatistics
+{
+    /// <summary>
+    /// Broker hostname, port and broker id
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Broker id (-1 for bootstraps)
+    /// </summary>
+    [JsonPropertyName("nodeid")]
+    public long NodeId { get; set; }
+
+    /// <summary>
+    /// Broker hostname (e.g. "example.com:9092").
+    /// </summary>
+    [JsonPropertyName("nodename")]
+    public string NodeName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Broker source (learned, configured, internal, logical)
+    /// </summary>
+    [JsonPropertyName("source")]
+    public string Source { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Broker state (INIT, DOWN, CONNECT, AUTH, APIVERSION_QUERY, AUTH_HANDSHAKE, UP, UPDATE)
+    /// </summary>
+    [JsonPropertyName("state")]
+    public string State { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Time since last broker state change (microseconds)
+    /// </summary>
+    [JsonPropertyName("stateage")]
+    public long StateAgeMicroseconds { get; set; }
+
+    /// <summary>
+    /// Partitions handled by this broker handle.
+    /// </summary>
+    [JsonPropertyName("toppars")]
+    public Dictionary<string, TopicPartition> TopicPartitions { get; } = new();
+}
---
+++ src/Epam.Kafka/Stats/JsonContext.cs
@@ -0,0 +1,31 @@
+﻿// Copyright © 2024 EPAM Systems
+
+using System.Text.Json;
+
+using System.Text.Json.Serialization;
+
+using Confluent.Kafka;
+
+namespace Epam.Kafka.Stats;
+
+[JsonSourceGenerationOptions(JsonSerializerDefaults.Web,
+    PreferredObjectCreationHandling = JsonObjectCreationHandling.Populate,
+    UseStringEnumConverter = true,
+    Converters = new[] { typeof(PartitionConverter) })]
+[JsonSerializable(typeof(Statistics))]
+internal partial class StatsJsonContext : JsonSerializerContext
+{
+    public class PartitionConverter : JsonConverter<Partition>
+    {
+
+        public override Partition Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return new Partition(reader.GetInt32());
+        }
+
+        public override void Write(Utf8JsonWriter writer, Partition value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(value.Value);
+        }
+    }
+}
